1ee84d40d411a34bb6a89f45113bc60050b3a881
ammarqassab <ammarqassab97@gmail.com>
2022-08-22 17:27:00 +0300

    add ayat

--- src/components/QuranSections/QuranSections.js
+++ src/components/QuranSections/QuranSections.js
@@ -17,7 +17,7 @@ const QuranSections = () => {
                 {quranalkarim &&
                 quranalkarim.map((iteme, index) => (
                     <div className='col m50 l25 padding center xlarge textc-5 hover-textc-2' key={index}>
-                        <Link href={iteme.link}>
+                        <Link href={iteme.link} prefetch={false}>
                             <div className='app-box-shadow hover-app-box-shadow padding-small pointer'>
                                 <a className=' text-decoration-none'>{`(${iteme.number})`} قراءة {iteme.name}</a>
                             </div>
