ca50285cc8c2fae5e0c78e59e67c85e2b532fc8f
ammarqassab <ammarqassab97@gmail.com>
2022-08-23 18:51:46 +0300

    add ayat

--- src/components/PageTutorial/SidebarTutorial/SidebarTutorial.js
+++ src/components/PageTutorial/SidebarTutorial/SidebarTutorial.js
@@ -10,14 +10,14 @@ const SidebarTutorial = ({functaggle}) => {
 
     return (
         <div>
-            <div className="sidebar bar-block leftbar borderc-3 medium">
+            <div className="sidebar bar-block leftbar borderc-3 large">
                 <a className='fas fa-times bar-item button border-bottom textc-2 hover-textc-2 center borderc-3  text-decoration-none' onClick={() => functaggle()}></a>
                 <Link href='/'>
                     <a className='fas fa-home bar-item button border-bottom textc-2 hover-textc-2 center borderc-3  text-decoration-none'> Home</a>
                 </Link>
                 {pages &&
                 pages.map((iteme, index) => (
-                    <Link href={iteme.link} key={index}>
+                    <Link href={iteme.link} prefetch={false} key={index}>
                         <a className={`bar-item button border-bottom textc-2 hover-textc-2 rtl borderc-3  text-decoration-none ${iteme.link == pname ? ' bgc-3':''}`}>{iteme.name}</a>
                     </Link>
                 ))
--- src/components/QuranPage/QuranPage.js
+++ src/components/QuranPage/QuranPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const QuranPage = ({ children }) => {
 
@@ -8,11 +9,21 @@ const QuranPage = ({ children }) => {
 
     const pname = useRouter().asPath;
     let pageinf = [];
+    let pagenext = null;
+    let pageprev = null;
 
     if(quranalkarim) {
         for(let i in quranalkarim) {
             if(quranalkarim[i].link == pname) {
+                if(i > 0) {
+                    pageprev = [];
+                    pageprev.push(quranalkarim[i - 1]);
+                }
                 pageinf.push(quranalkarim[i]);
+                if(quranalkarim.length - 1 > i) {
+                    pagenext = [];
+                    pagenext.push(quranalkarim[++i]);
+                }
                 break;
             }
         }
@@ -27,6 +38,22 @@ const QuranPage = ({ children }) => {
                 <div className='center xxlarge textc-4 padding'> بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ </div>
                 <div className=' margin-bottom xlarge quran'>
                     {children}
+                </div>
+            </div>
+            <div className='center margin-top'>
+                <div className=' bar'>
+                    <nav className=' row flex-nowrap'>
+                        {pagenext ?
+                        <Link href={pagenext[0].link}><a className='bar-item btn margin round-xlarge text-decoration-none medium'>قراءة {pagenext[0].name}</a></Link>
+                        :
+                        <a className='bar-item btn margin round-xlarge text-decoration-none medium'>لا يوجد سورة تالية</a>
+                        }
+                        {pageprev ?
+                        <Link href={pageprev[0].link}><a className='bar-item btn margin round-xlarge text-decoration-none medium'>قراءة {pageprev[0].name}</a></Link>
+                        :
+                        <a className='bar-item btn margin round-xlarge text-decoration-none medium'>لا يوجد سورة سابقة</a>
+                        }
+                    </nav>
                 </div>
             </div>
         </div>
