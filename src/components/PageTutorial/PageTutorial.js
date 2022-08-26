84249433d159bf1f99564188a61a52f5c1aff3c2
ammarqassab <ammarqassab97@gmail.com>
2022-08-26 23:17:32 +0300

    update SEO

--- src/components/PageTutorial/PageTutorial.js
+++ src/components/PageTutorial/PageTutorial.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { useRouter } from 'next/router'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { FetchApi } from '../../Api/FetcherApi'
 import { addPages } from '../../Store/navigationSlice'
 import SidebarStickyTutorial from './SidebarTutorial/SidebarStickyTutorial'
 import SidebarTutorial from './SidebarTutorial/SidebarTutorial'
-import CommonSEO from '../SEO/SEO'
 
 const PageTutorial = ({ children,  apiLocalUrl}) => {
-
-    const pages = useSelector(state => state.navigation.pages)
     const dispatch = useDispatch();
 
     const api = FetchApi(apiLocalUrl);
@@ -32,21 +28,8 @@ const PageTutorial = ({ children,  apiLocalUrl}) => {
         }
     }
 
-    const pname = useRouter().asPath;
-    let pageinf = [];
-
-    if(pages) {
-        for(let i in pages) {
-            if(pages[i].link == pname) {
-                pageinf.push(pages[i]);
-                break;
-            }
-        }
-    }
-
     return (
         <>
-            {pageinf.length >0 && <CommonSEO title={`${pageinf[0].name}`} description={`${pageinf[0].description}`} keywords={`${pageinf[0].name}, ${pageinf[0].sections}, ${pageinf[0].tutorial}`}/>}
             <div className=' display-container width-mdx'>
                 <div id='idtutorual' className=' height-con' >
                     <SidebarStickyTutorial functaggle={functaggle}/>
